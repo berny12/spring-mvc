14018a57094a382dee147c1163edc4da04ca5a1a
edgar bernal remoto <tecualoyaster@gmail.com>
2014-09-13 12:38:22 -0500

    configuracion mvc
    motor de spring-mvc final, con un controller y un vista

--- src/test/java/com/synergyj/curso/web/services/test/ServicioMonitoreoTest.java
+++ src/test/java/com/synergyj/curso/web/services/test/ServicioMonitoreoTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.synergyj.curso.web.services.BeanMonitoreo;
@@ -14,7 +15,7 @@ import com.synergyj.curso.web.services.ServicioMonitoreo;
 
 //para cambiar el motoro de pruebas unitarias
 @RunWith(SpringJUnit4ClassRunner.class)
-@Configuration("/webApplicationContext.xml")
+@ContextConfiguration("/webApplicationContext.xml")
 public class ServicioMonitoreoTest {
 
 	@Autowired
