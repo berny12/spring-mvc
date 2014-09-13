a3767d2f67f04eec554e612a161fd19fc3125939
edgar bernal remoto <tecualoyaster@gmail.com>
2014-09-13 11:32:06 -0500

    configuracion del webcontext
    configuracon de webcontext

---
+++ src/test/java/com/synergyj/curso/web/services/test/ServicioMonitoreoTest.java
@@ -0,0 +1,29 @@
+package com.synergyj.curso.web.services.test;
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.synergyj.curso.web.services.BeanMonitoreo;
+import com.synergyj.curso.web.services.ServicioMonitoreo;
+
+
+//para cambiar el motoro de pruebas unitarias
+@RunWith(SpringJUnit4ClassRunner.class)
+@Configuration("/webApplicationContext.xml")
+public class ServicioMonitoreoTest {
+
+	@Autowired
+	ServicioMonitoreo servicioMonitoreo;
+	@Test
+	public void testServicioMonitoreo() {
+		Assert.assertNotNull(servicioMonitoreo);
+		BeanMonitoreo bean = servicioMonitoreo.regresaMonitoreo();
+		Assert.assertNotNull(bean);
+	}
+
+}
